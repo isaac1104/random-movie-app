03e5891166d64b94a92492708dd9f31549293ba2
Isaac Kwon <isaac-kwon@hotmail.com>
2018-07-26 11:00:20 -0700

    search form auto selects all value when clicked

--- client/src/components/SearchForm/FormField.js
+++ client/src/components/SearchForm/FormField.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const FormField = field => {
+  const handleFocus = event => {
+    event.target.select();
+  }
+
   const { meta: { touched, error } } = field;
-  const className = `form-control ${touched && error ? "is-invalid" : ""}`;
+  const className = `form-control ${touched && error ? 'is-invalid' : ''}`;
   return (
-    <div className="form-group">
-      <label className="bmd-label-floating">{field.label}</label>
+    <div className='form-group'>
+      <label className='bmd-label-floating'>{field.label}</label>
       <input
-        className={className}
-        type="input"
         {...field.input}
-        autoComplete="off"
+        className={className}
+        placeholder='Enter a movie title for details'
+        autoComplete='off'
+        onFocus={handleFocus}
       />
-      <div className="invalid-feedback">
-        {touched ? error : ""}
+      <div className='invalid-feedback'>
+        {touched ? error : ''}
       </div>
     </div>
   );
