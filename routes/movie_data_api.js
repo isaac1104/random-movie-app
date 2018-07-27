2f67a97e1935542bfbfb349f4ffb66d4d1a8efb5
isaac1104 <isaac-kwon@hotmail.com>
2018-07-26 18:18:13 -0700

    build

--- routes/movie_data_api.js
+++ routes/movie_data_api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = app => {
   app.get('/api/movie_data', async (req, res) => {
-    const request = await axios.get(`https://www.omdbapi.com/?apikey=ac19108c&t=${req.query.title}`);
+    const request = await axios.get(`https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${req.query.title}`);
     const { data } = request;
     res.send(data);
   });
